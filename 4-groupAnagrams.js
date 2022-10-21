4682f5a3da7796b6c70e8b1eb8b15b3f3ced0e30
Ahkal <bp@sojourn.llc>
2022-10-21 13:34:04 -0400

    Gets better understanding of grouping anagrams.

--- 4-groupAnagrams.js
+++ 4-groupAnagrams.js
@@ -1,12 +1,25 @@
 const groupAnagram = (strs) => {
+  // create empty object to use later
   let obj = {};
+  // iterate through strs[]
+  // (for of)
   for (let i of strs) {
+    // i is the name for each entry in our array.
+    // cut out each character
+    // sort abc order
+    // join letters together
+    // store inside a variable.
     let key = i.split("").sort().join("");
+    // condition: if this key doesn't exist in the object, create it and store the item as an array.
     if (!obj[key]) {
       obj[key] = [i];
     } else {
       obj[key].push(i);
     }
   }
+  console.log(obj);
+  // if this key does exist push the value.
   return Object.values(obj);
 };
+
+console.log(groupAnagram(["eat", "tea", "tan", "ate", "nat", "bat"]));
--- 4-validAnagram.js
+++ 4-validAnagram.js
@@ -1,6 +1,8 @@
 const isAnagram = (a, b) => {
+  // store the length of each array: a & b
   let lenA = a.length;
   let lenB = b.length;
+  // if the anagrams are not the same length return false.
   if (lenA !== lenB) return false;
   let table = {};
   for (let letter of a) {
